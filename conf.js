939c435cec45b65e980406efd54b259e5de399b0
rospod <57697238+rospod@users.noreply.github.com>
2020-03-18 16:38:51 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -38,7 +38,7 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        //'node-cfworker-2.etherdream.com': 4,
+//         'node-cfworker-2.etherdream.com': 4,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
@@ -46,6 +46,8 @@ jsproxy_config({
         //'b.hehe.workers.dev': 1,
         //'b.lulu.workers.dev': 1,
         //'b.jsproxy.workers.dev': 1,
+        'proxy.ninjarospod.workers.dev': 1
+        
       }
     }
   },
